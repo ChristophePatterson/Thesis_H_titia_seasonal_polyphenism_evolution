f070ee467aba5ede2281b26124a9a412476d3fe8
ChristophePatterson <christophepatterson@outlook.com>
2025-02-03 14:43:00 +0000

    Complete first assignment code for WGS and phenotype data

--- scripts/WGS/WGS_cluster_exploration.R
+++ scripts/WGS/WGS_cluster_exploration.R
@@ -18,12 +18,12 @@ phen<-read.csv("data/MASTER_titia_phenotypes_v1.0.csv")
 
 ## Get samples that were included WGS phylogeny 
 # Hydro basin 5
-WGS_hydro5 <- read.table("data/SNPs/SNAPP/Hetaerina_titia_ddRAD_titia_dg-SNAPP-hydro_5_max_cov.txt", header = T)
+WGS_hydro5 <- read.table("data/SNPs/SNAPP/titia.mysnps-SNAPP-hydro_5_max_cov.txt", header = T)
 # Get hydrobasin name
 WGS_hydro5$hydro5 <- substr(WGS_hydro5$species, 1, 10)
 #Hydrobasin4
 ## Get samples that were included WGS phylogeny 
-WGS_hydro4 <- read.table("data/SNPs/SNAPP/Hetaerina_titia_ddRAD_titia_dg-SNAPP-hydro_4_max_cov.txt", header = T)
+WGS_hydro4 <- read.table("data/SNPs/SNAPP/titia.mysnps-SNAPP-hydro_4_max_cov.txt", header = T)
 # Get hydrobasin name
 WGS_hydro4$hydro4 <- substr(WGS_hydro4$species, 1, 10)
 
@@ -57,21 +57,29 @@ clusters[[4]] <- c(7050047850, 7050736840, 7050047980, 7050048620, 7050047970, 7
 clusters[[5]] <- c(7050668250, 7050741770, 7050682360)
 clusters[[6]] <- c(7050822250, 7050823030)
 clusters[[7]] <- c(7050050000, 7050050930, 7050849600)
-clusters[[8]] <- c(7050050640)
+clusters[[8]] <- c(7050050640, 7050050650)
 clusters[[9]] <- c(7050054660, 7050054670)
 clusters[[10]] <- c(7050043860, 7050043870, 7050045480)
 clusters[[11]]  <- c(7050045900)
-clusters[[12]]  <- c(7050041410, 7050453940, 7050494360)
-clusters[[13]]  <- c(7050609780)
-clusters[[14]]  <- c(7050511950, 7050531210)
+clusters[[12]]  <- c(7050046750)
+clusters[[13]]  <- c(7050041410, 7050453940, 7050494360)
+clusters[[14]]  <- c(7050609780)
+clusters[[15]]  <- c(7050511950, 7050531210)
 
 # All hydrobasins included in cluster
 all_hydro <- unlist(clusters)
 # Any WGS samples not used in clusters?
 miss_hydro <- WGS_hydro5[which(!WGS_hydro5$hydro5%in%all_hydro),]$hydro5
+# Hydrobasins that have been assigned to basin without direct WGS samples
+extra_hydro <- all_hydro[!all_hydro%in%WGS_hydro5$hydro5]
+
 p <- ggplot(phen[!is.na(phen$WGS_hydro_ID_5),]) +
   geom_sf(data = world_map) +
+  geom_sf(data = hydrobasins_5[hydrobasins_5$HYBAS_ID%in%all_hydro,], fill = "lightgreen", col = "black") +
   geom_sf(data = hydrobasins_5[hydrobasins_5$HYBAS_ID%in%miss_hydro,], fill = "grey60", col = "black") +
+  geom_sf(data = hydrobasins_5[hydrobasins_5$HYBAS_ID%in%extra_hydro,], fill = "purple1", col = "black") +
+  geom_sf_text(data = hydrobasins_5[hydrobasins_5$HYBAS_ID%in%all_hydro,], aes(label=HYBAS_ID), size = 1) +
+  geom_sf_text(data = hydrobasins_5[hydrobasins_5$HYBAS_ID%in%extra_hydro,], aes(label=HYBAS_ID), size = 1) +
   geom_sf_text(data = hydrobasins_5[hydrobasins_5$HYBAS_ID%in%miss_hydro,], aes(label=HYBAS_ID), size = 1)
 
 ggsave("plots/cluster_exploration/unassigned_WGS_samples.pdf", p)
@@ -105,3 +113,5 @@ p <- ggplot(phen) +
 
 ggsave("plots/cluster_exploration/clusters_mapped.pdf", q+p, height = 10,width = 15)
 
+
+#### Assigning samples outside of basins with 
