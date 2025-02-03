f7bb2ffd4865f8f45a7a8d65de4081764368b83a
ChristophePatterson <christophepatterson@outlook.com>
2025-02-03 15:12:41 +0000

    Further assignment code of clusters

--- scripts/WGS/WGS_cluster_exploration.R
+++ scripts/WGS/WGS_cluster_exploration.R
@@ -103,15 +103,49 @@ q <- ggplot(phen[!is.na(phen$WGS_hydro_ID_5),]) +
   geom_sf(data = hydrobasins_5[hydrobasins_5$HYBAS_ID%in%phen$HYBAS_ID_5,], fill = "grey60", col = "black") +
   geom_sf(data = hydrobasins_5[hydrobasins_5$HYBAS_ID%in%all_hydro,], fill = "grey40", col = "black") +
   geom_sf_text(data = hydrobasins_5[hydrobasins_5$HYBAS_ID%in%phen$HYBAS_ID_5,], aes(label = HYBAS_ID),size=0.5) +
-  geom_point(aes(longitude, latitude,col = as.factor(WGS_hydro_ID_5)), show.legend = F, size=0.1)
-  #geom_text_repel(data = clust_summ, aes(mn.lon, mn.lat, label = by),min.segment.length = 0,force = 10)
+  geom_point(aes(longitude, latitude,col = as.factor(WGS_hydro_ID_5)), show.legend = F, size=0.1) +
+  geom_text_repel(data = clust_summ, aes(mn.lon, mn.lat, label = by),min.segment.length = 0,force = 50, nudge_x = -10)
 
 p <- ggplot(phen) +
   geom_point(aes(x = Julian.date, y = draft.estimate.prop.pigment, col = as.factor(WGS_hydro_ID_5)), size=0.2) +
   facet_wrap(~WGS_hydro_ID_5)
 
+ggsave("plots/cluster_exploration/clusters_mapped.pdf", q + p, height = 10,width = 15)
 
-ggsave("plots/cluster_exploration/clusters_mapped.pdf", q+p, height = 10,width = 15)
 
+### Building up clusters to basins outside of the sampled ones
 
-#### Assigning samples outside of basins with 
+## Identified clusters
+# Pre-lim list of identified clusters
+clusters <- list()
+clusters[[1]] <- c(7050000010)
+clusters[[2]] <- c(7050004220, 7050005420, 7050004670)
+clusters[[3]] <- c(7050002710)
+clusters[[4]] <- c(7050047850, 7050736840, 7050047980, 7050048620, 7050047970, 7050683900)
+clusters[[5]] <- c(7050668250, 7050741770, 7050682360)
+clusters[[6]] <- c(7050822250, 7050823030)
+clusters[[7]] <- c(7050050000, 7050050930, 7050849600)
+clusters[[8]] <- c(7050050640, 7050050650)
+clusters[[9]] <- c(7050054660, 7050054670)
+clusters[[10]] <- c(7050043860, 7050043870, 7050045480)
+clusters[[11]]  <- c(7050045900)
+clusters[[12]]  <- c(7050046750)
+clusters[[13]]  <- c(7050041410, 7050453940, 7050494360)
+clusters[[14]]  <- c(7050609780)
+clusters[[15]]  <- c(7050511950, 7050531210)
+
+# All hydrobasins included in cluster
+all_hydro <- unlist(clusters)
+# Any WGS samples not used in clusters?
+miss_hydro <- WGS_hydro5[which(!WGS_hydro5$hydro5%in%all_hydro),]$hydro5
+# Hydrobasins that have been assigned to basin without direct WGS samples
+extra_hydro <- all_hydro[!all_hydro%in%WGS_hydro5$hydro5]
+
+p <- ggplot(phen[!is.na(phen$WGS_hydro_ID_5),]) +
+  geom_sf(data = world_map) +
+  geom_sf(data = hydrobasins_5[hydrobasins_5$HYBAS_ID%in%phen$HYBAS_ID_5,], fill = "grey60", col = "black") +
+  geom_sf(data = hydrobasins_5[hydrobasins_5$HYBAS_ID%in%all_hydro,], fill = "lightgreen", col = "black") +
+  geom_sf(data = hydrobasins_5[hydrobasins_5$HYBAS_ID%in%extra_hydro,], fill = "purple1", col = "black") +
+  geom_sf_text(data = hydrobasins_5[hydrobasins_5$HYBAS_ID%in%phen$HYBAS_ID_5,], aes(label=HYBAS_ID), size = 0.5)
+
+ggsave("plots/cluster_exploration/assigning_pheno_by_WGS_samples.pdf", p)
