70def8cdbb096c43c88da8e3aa9e45282c7b4d45
Christophe Patterson <christophepatterson@outlook.com>
2026-03-04 10:43:10 +0000

    WGS SNAPP running on ada

--- scripts/WGS/SNAPP_run_array_hydro_basins_Chap_4_WGS.sh
+++ scripts/WGS/SNAPP_run_array_hydro_basins_Chap_4_WGS.sh
@@ -1,42 +1,57 @@
 #!/bin/bash
+# Christophe Patterson
+# 10/04/25
+# for running on the UoN HPC Ada
 
-#SBATCH -c 124 
-#SBATCH --mem=15G            # memory required, in units of k,M or G, up to 250G.
-#SBATCH --gres=tmp:15G       # $TMPDIR space required on each compute node, up to 400G.
-#SBATCH -t 71:59:00         # time limit in format dd-hh:mm:ss
-#SBATCH --output=/nobackup/tmjj24/slurm-%x.%j.out
+#SBATCH --partition=defq
+#SBATCH --nodes=1
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=84
+#SBATCH --mem=80g
+#SBATCH --time=167:00:00
+#SBATCH --array=1
+#SBATCH --job-name=SNAPP_WGS
+#SBATCH --output=/gpfs01/home/mbzcp2/slurm_outputs/slurm-%x-%j.out
 
 # Commands to execute start here
-module purge
-module load gcc/native
-module load gdal
-module load geos/3.10.1
-module load r/4.2.1
-module load bioinformatics
+# OLD HAMILTON MODULES
+###### module purge
+###### module load gcc/native
+###### module load gdal
+###### module load geos/3.10.1
+###### module load r/4.2.1
+###### module load bioinformatics
+# NEW ADA modules
+module load R-uoneasy/4.2.1-foss-2022a
+module load ruby-uoneasy/3.4.2-GCCcore-13.3.0
+
 # module load beast
 # May need to run the code
 # /home/tmjj24/apps/beast/bin/packagemanager -add SNAPP
 
+base_dir="/gpfs01/home/$USER/code/Github/"
+
+hydro="4"
 # Output directory
-output_dir=(/home/tmjj24/scripts/Github/Thesis_H_titia_seasonal_polyphenism_evolution/data/SNAPP/Hydro_5_WGS_max_cov)
-input_dir=(/home/tmjj24/scripts/Github/Thesis_H_titia_seasonal_polyphenism_evolution/data/SNPs/SNAPP)
+output_dir=(${base_dir}/Thesis_H_titia_seasonal_polyphenism_evolution/data/SNAPP/Hydro_${hydro}_WGS_max_cov)
+input_dir=(${base_dir}/Thesis_H_titia_seasonal_polyphenism_evolution/data/SNPs/SNAPP)
 mkdir -p $output_dir
 
 # Input file prefix
-phy_file=(titia.mysnps-SNAPP-hydro_5_max_cov)
+phy_file=(titia.mysnps-SNAPP-hydro_${hydro}_max_cov)
 
 cd $output_dir
 
 # Run ruby script for creating xml SNAPP config file
 # Uses https://github.com/mmatschiner/tutorials/blob/master/divergence_time_estimation_with_snp_data/README.md
 
-ruby /home/tmjj24/scripts/job_scripts/Master-demulitiplex-scripts/ddRAD_Durham_and_Sheffield_CUAJ/SNAPP/snapp_prep.rb -p $input_dir/$phy_file.phy -t $input_dir/$phy_file.txt \
--c $input_dir/$phy_file.con.txt -l 1000000 -x $phy_file.xml -o $phy_file
+ruby ${base_dir}/Thesis_H_titia_seasonal_polyphenism_evolution/scripts/snapp_prep.rb -p $input_dir/$phy_file.phy -t $input_dir/$phy_file.txt \
+-c $input_dir/$phy_file.con.txt -l 1000000 -x $output_dir/$phy_file.xml -o $output_dir/$phy_file
 
 ## ALERT I HAVE CHANGED THE LOCATION OF BEAST FROM HOME TO NOBACKUP SINCE RUNNING THIS SCRIPT AS SUCH PREPARE FOR ERRORS
 
-/nobackup/tmjj24/apps/beast/bin/beast -threads 124 -overwrite $phy_file.xml > $phy_file.screen.log
+~/apps/beast/bin/beast -threads $SLURM_CPUS_PER_TASK -overwrite $output_dir/$phy_file.xml > $phy_file.screen.log
 
-/nobackup/tmjj24/apps/beast/bin/treeannotator -burnin 10 $phy_file.trees $phy_file.trees.Anon
+~/apps/beast/bin/treeannotator -burnin 10 $phy_file.trees $phy_file.trees.Anon
 
 cd ~
