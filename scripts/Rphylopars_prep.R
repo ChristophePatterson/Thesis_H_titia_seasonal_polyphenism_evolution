8bb5ed66da9b1ea63beaf99c5a80b566ad56e41a
Jonathan Drury <jonathan.p.drury@gmail.com>
2026-04-26 15:39:39 +0100

    no message

--- scripts/Rphylopars_prep.R
+++ scripts/Rphylopars_prep.R
@@ -1,5 +1,8 @@
 #this script calculates the peak and trough mean and standard error for each tip
 #current data transformation is a logit, subtracting 0.01 from each value so 1 values don't hit Inf
+
+#this version from 15 April 2026, new peak datas from gam with gaussian link on logit transformations
+
 setwd("~/Thesis_H_titia_seasonal_polyphenism_evolution")
 phen<-read.csv("data/MASTER_titia_phenotypes_v2.0_processed.csv")
 peak.trough.season <- read.csv("data/peak_trough_season_dates.csv")
@@ -7,30 +10,33 @@ peak.trough.season <- read.csv("data/peak_trough_season_dates.csv")
 
 
 lt<-LETTERS[1:8]
-tips.a.to.h<-c("7050000010-NB0103",  "7050002710-ZANAa05","7050054670-BALB05", "7050849600-TXRSa04",  "7050045480-ACSFc04","7050041410-RCJRa01","7050822250-CYa04" , "7050048620-CVa06" ) 
+#tips.a.to.h<-c("7050000010-NB0103",  "7050002710-ZANAa05","7050054670-BALB05", "7050849600-TXRSa04",  "7050045480-ACSFc04","7050041410-RCJRa01","7050822250-CYa04" , "7050048620-CVa06" ) 
 
 
 for(i in 1:length(lt)){
 	lt.sub<-subset(phen,CLUSTER==lt[i])
 	lt.peak.sub<-subset(lt.sub,Julian.date>=peak.trough.season[i,2],Julian.date<=peak.trough.season[i,3])
-	hw.trans.peak = log( (lt.peak.sub$proportion.pigmented-0.01) / (1-((lt.peak.sub$proportion.pigmented-0.01))))
+	hw.logit = log( (lt.peak.sub$proportion.pigmented-0.01) / (1-((lt.peak.sub$proportion.pigmented-0.01))))
+	hw.raw = lt.peak.sub$proportion.pigmented
 	
 	if(i ==1){
 	
-		out.mat = cbind(lt[i],hw.trans.peak,"peak")
+		out.mat = cbind(lt[i],hw.raw,hw.logit,"peak")
 	
 	} else {
 	
-		out.mat = rbind(out.mat, cbind(lt[i],hw.trans.peak,"peak"))
+		out.mat = rbind(out.mat, cbind(lt[i],hw.raw,hw.logit,"peak"))
 	}
 	
 	
 	lt.trough.sub<-subset(lt.sub,Julian.date>=287 | Julian.date<=91)
-	hw.trans.trough = log( (lt.trough.sub$proportion.pigmented-0.01) / (1-((lt.trough.sub$proportion.pigmented-0.01))))
-		out.mat = rbind(out.mat, cbind(lt[i],hw.trans.trough,"trough"))
+	hw.logit.trough = log( (lt.trough.sub$proportion.pigmented-0.01) / (1-((lt.trough.sub$proportion.pigmented-0.01))))
+	hw.raw.trough= lt.trough.sub$proportion.pigmented
+	out.mat = rbind(out.mat, cbind(lt[i],hw.raw.trough,hw.logit.trough,"trough"))
 }
 
-
+colnames(out.mat)<-c("species","hw.raw","hw.logit","time")
+out.mat<-as.data.frame(out.mat)
 
 
 write.csv(out.mat,file="rphylopars_data.csv")
